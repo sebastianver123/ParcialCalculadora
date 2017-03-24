ba124cc72ccb64ad9eed9eb49a4ffcb4bf229c48
estudiantes <estudiantes@labing00>
2017-03-24 17:13:07 -0500

    Operaciones

---
+++ ParcialCalculadora/src/parcialcalculadora/Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialcalculadora;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Calculadora {
+    
+  
+    
+    public double suma(double num1, double num2){
+        
+        double suma;
+        suma= num1+num2;
+        return suma;
+        
+    }
+    public double resta(double num1, double num2){
+        
+        return num1-num2;
+        
+    }
+    public double multiplicacion(double num1, double num2){
+        
+        return num1*num2;
+        
+    }
+    public double division(double num1, double num2){
+        
+        return num1/num2;
+        
+    }
+
+  
+
+ 
+    
+    
+    
+}
